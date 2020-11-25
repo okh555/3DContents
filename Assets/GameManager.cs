92894b1aa68517e02737a062ff44219d4e71621b
Paul Kim <poopidoopi97@naver.com>
2020-11-25 22:21:26 +0900

    Map1

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -17,8 +17,6 @@ public class GameManager : MonoBehaviour
     void Update()
     {
     
-
-
         
     }
 }
---
+++ Assets/PlatformAttach.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlatformAttach : MonoBehaviour
+{
+    public GameObject Player;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
