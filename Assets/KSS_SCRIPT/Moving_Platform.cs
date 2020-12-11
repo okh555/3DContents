8b8748eb28755b221c52a1316394a2022166ce9f
Paul Kim <poopidoopi97@naver.com>
2020-12-11 20:03:48 +0900

    Map1 , Map2
    Map1 Map2 일단 구현 완료

---
+++ Assets/KSS_SCRIPT/Moving_Platform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using UnityEngine;
+
+public class Moving_Platform : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+            if(other.gameObject.tag == "platform")
+             Player.transform.parent = transform;
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+}
