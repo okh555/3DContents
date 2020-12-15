e62dd6a37752224d9131d30c31425dd7afcf5cdd
Paul Kim <poopidoopi97@naver.com>
2020-12-15 21:22:32 +0900

    123

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,22 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Media;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     private Animator animator;
-
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
     
-        
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            player.transform.position = new Vector3(0,0,0);
+            Debug.Log("asdasd111");
+            //SceneManager.LoadScene("3DContents");
+        }
+    }
+
 }
