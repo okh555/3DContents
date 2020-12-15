be09051aefdc914e5d7ac8f2fb13f2188cfb74b3
Paul Kim <poopidoopi97@naver.com>
2020-12-15 15:32:19 +0900

    map transition

---
+++ Assets/endTutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class endTutorial : MonoBehaviour
+{
+    public AudioClip soundSE;
+    AudioSource aud;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //this.aud.PlayOneShot(this.soundSE);
+            SceneManager.LoadScene("Map1");
+        }
+    }
+}
